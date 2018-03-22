d767f2ce1767685d87d26f9bf00bddc1a85836cc
Lukasz Klis <lukaszklis@users.noreply.github.com>
2018-03-22 11:25:58 +0100

    Add speaker gallery & simple speakers page (#111)
    * Implement speaker gallery component
    
    * Refactor speaker gallery component
    
    * Fix styles for stage
    
    * Add a simple Speakers page
    
    * Fix sortObjects
    
    * adjust wording
    
    * More pleasant margin on speaker title (I’ll discuss with Nicolas if needed)
    
    * remove CoC block on main landing page (it is now linked to in the footer)

---
+++ templates/filters/sortObjects.js
@@ -0,0 +1,26 @@
+const objectPath = require('object-path');
+
+module.exports = function(
+  arr,
+  keyPath,
+  defaultValue = 0,
+  compareAs = 'number'
+) {
+  if (!arr || arr.length < 1) {
+    return [];
+  }
+
+  return arr.slice(0).sort((objA, objB) => {
+    const a = objectPath.get(objA, keyPath, defaultValue);
+    const b = objectPath.get(objB, keyPath, defaultValue);
+
+    switch (compareAs) {
+      case 'number':
+        return a - b;
+      case 'string':
+        return a.localeCompare(b);
+      case 'date':
+        throw new Error('sort-object: compareAs=date not yet implemented.');
+    }
+  });
+};
