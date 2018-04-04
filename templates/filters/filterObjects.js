1d4216dc8aee7ed2db12618653f0fac1138ae4ce
Lukasz Klis <lukasz@klis.pl>
2018-04-04 08:53:27 +0200

    Fix filterObjects for empty input array

--- templates/filters/filterObjects.js
+++ templates/filters/filterObjects.js
@@ -5,5 +5,9 @@ module.exports = function (
   keyPath,
   filterBy
 ) {
+  if (!arr || arr.length < 1) {
+    return [];
+  }
+
   return arr.slice(0).filter(item => objectPath.get(item, keyPath) === filterBy);
 };
